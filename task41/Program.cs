25f0f95c024331b095d204a37609d5f29d99e1ee
ilnur-Git <mazel08@yandex.ru>
2023-03-14 22:09:52 +0300

    Домашка семинар 6

---
+++ task41/Program.cs
@@ -0,0 +1,48 @@
+﻿// // Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+
+List<int> lisNum = new List<int>();
+Console.WriteLine("Введите число,_____ для введения следующего числа нажмите [Enter]");
+
+
+List<int> listСompletion(List<int> lisNum)
+{
+    string close = "";
+
+    while (close != "00")
+    {
+        Console.WriteLine("введите число   (для выхода введите 00)");
+        close = Console.ReadLine();
+        int number = Convert.ToInt32(close);
+        lisNum.Add(number);
+    }
+    return lisNum;
+}
+
+void countingNum(List<int> lisNum)
+{
+
+    int count  = 0;
+    foreach (var num in lisNum)
+    {
+        if (num > 0) { count++; }
+    }
+    Console.WriteLine($"\nчисел больше 0 ---- {count}");
+}
+
+
+void show(List<int> LisNum)
+{
+    foreach (var num in lisNum)
+    {
+        Console.Write($"[{num}]");
+    } 
+}
+
+
+show(listСompletion(lisNum));
+countingNum(lisNum);
